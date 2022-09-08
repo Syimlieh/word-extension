f24604c9233078ea648c2d06c0eb7f189f741381
Fleming <flemingsyiemlieh8899@gmail.com>
2022-09-09 00:15:51 +0530

    redesign

--- content-script.js
+++ content-script.js
@@ -1,14 +1,55 @@
-console.log("Hello World");
-
-window.addEventListener("mouseup", mouseUpped);
-
-function mouseUpped() {
-  let selectedText = window.getSelection().toString();
-  console.log("SElected Text ", selectedText);
-  if (selectedText.length > 0) {
-    let message = {
-      text: selectedText,
-    };
-    chrome.runtime.sendMessage(message);
+chrome.runtime.onMessage.addListener((message, sender) => {
+  if (message?.from === "popup" && message?.info === "connection_start") {
+    console.log("message", message);
+    window.addEventListener("mouseup", mouseUpped);
+    function mouseUpped() {
+      setTimeout(() => {
+        let selectedText = window.getSelection().toString();
+        console.log("SElected Text ", selectedText);
+        if (selectedText.length > 0) {
+          let message = {
+            text: selectedText,
+          };
+          console.log("Message: ", message);
+          chrome.runtime.sendMessage(message);
+          chrome.storage.local.get("word_defination", (response) => {
+            chrome.storage.local.set({ word_defination: message });
+          });
+        }
+      }, 500);
+    }
   }
-}
+});
+
+// function mouseUpped() {
+//   let selectedText = window.getSelection().toString();
+//   console.log("SElected Text ", selectedText);
+//   if (selectedText.length > 0) {
+//     let message = {
+//       text: selectedText,
+//     };
+//     chrome.runtime.sendMessage(message);
+//   }
+// }
+// chrome.runtime.onConnect.addListener(function (port) {
+//   port.onMessage.addListener(function (message) {
+//     console.log("message", message);
+//     if (port.name === "word") {
+//       const idToQuery = message.id;
+//       console.log("query", idToQuery);
+//       window.addEventListener("mouseup", function () {
+//         let selectedText = this.window.getSelection().toString();
+//         if (selectedText.length > 0) {
+//           let message = {
+//             text: selectedText,
+//           };
+//           postMessage({
+//             message,
+//           });
+//           chrome.runtime.sendMessage(message);
+//         }
+//       });
+//     }
+//     console.log(message);
+//   });
+// }); //
--- popup.js
+++ popup.js
@@ -1,4 +1,4 @@
-(() => {
+document.addEventListener("DOMContentLoaded", () => {
   //   var background = chrome.extension.getBackgroundPage();
 
   //   console.log("word", background);
@@ -6,17 +6,68 @@
   let elem = document.createElement("p");
   elem.innerHTML = "word";
   document.getElementById("popword").appendChild(elem);
+  const connectButton = document.getElementById("popwordConnectButton");
   // let page = chrome.extension.getBackgroundPage();
   // page.foo();
+  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+    const tab = tabs[0];
+    if (tab.url === undefined || tab.url.indexOf("chrome") == 0) {
+      elem.innerHTML =
+        '<span style="font-family: lobster, sans-serif">Eye Dropper</span> can\'t access <i>Chrome pages</i>';
+    } else if (tab.url.indexOf("file") === 0) {
+      elem.innerHTML =
+        '<span style="font-family: lobster, sans-serif">Eye Dropper</span> can\'t access <i>local pages</i>';
+    } else {
+      connectButton.onclick = async function (e) {
+        chrome.tabs.sendMessage(tabs[0].id, {
+          from: "popup",
+          info: "connection_start",
+        });
+        window.close();
+      };
+    }
+  });
 
   //listening for message from content scripts
-  chrome.runtime.onMessage.addListener(function (
-    request,
-    sender,
-    sendResponse
-  ) {
-    if (request) {
-      let url = `https://api.dictionaryapi.dev/api/v2/entries/en/${request.text}`;
+  // chrome.runtime.onMessage.addListener(function (
+  //   request,
+  //   sender,
+  //   sendResponse
+  // ) {
+  //   if (request) {
+  //     let url = `https://api.dictionaryapi.dev/api/v2/entries/en/${request.text}`;
+  //     async function apiCall() {
+  //       const response = await fetch(url);
+  //       const responseJson = await response.json();
+  //       console.log(responseJson);
+  //       return await responseJson;
+  //     }
+
+  //     apiCall()
+  //       .then((movies) => {
+  //         if (movies) {
+  //           console.log(movies);
+  //           const mappingData =
+  //             movies[0]?.meanings[0]?.definitions[0]?.definition;
+  //           console.log(mappingData);
+  //           elem.innerHTML = request ? mappingData : "wording";
+  //         }
+  //       })
+  //       .catch((error) => {
+  //         console.log(error.message); // 'An error has occurred: 404'
+  //       });
+  //   }
+
+  //   if (request.type) {
+  //     document.body.style.backgroundColor = "green";
+  //     sendResponse({ status: "done" });
+  //   }
+  // });
+
+  chrome.storage.local.get(["word_defination"], (response) => {
+    console.log("response: " + JSON.stringify(response));
+    if (response.word_defination) {
+      let url = `https://api.dictionaryapi.dev/api/v2/entries/en/${response.word_defination.text}`;
       async function apiCall() {
         const response = await fetch(url);
         const responseJson = await response.json();
@@ -31,17 +82,12 @@
             const mappingData =
               movies[0]?.meanings[0]?.definitions[0]?.definition;
             console.log(mappingData);
-            elem.innerHTML = request ? mappingData : "wording";
+            elem.innerHTML = response.word_defination ? mappingData : "wording";
           }
         })
         .catch((error) => {
           console.log(error.message); // 'An error has occurred: 404'
         });
     }
-
-    if (request.type) {
-      document.body.style.backgroundColor = "green";
-      sendResponse({ status: "done" });
-    }
   });
-})();
+});
